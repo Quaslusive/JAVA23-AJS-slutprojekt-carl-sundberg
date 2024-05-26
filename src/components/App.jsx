dae648acd6345aa9fcd952d4098e39cfdcb01049
Pawlo Picasso <Quaslusive@protonmail.com>
2024-05-26 20:02:15 +0200

    v0.1.0.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,17 @@
-/*
 import TaskBoard from "./TaskBoard";
 import Task from "./Task";
 import AddTaskForm from "./AddTaskForm";
-import React from "react";
-
-
-function App() {
-    return (
-        <>
-            <h2>App</h2>
-            <TaskBoard/>
-            <Task/>
-            <AddTaskForm/>
-        </>
-      );
-}
-
-export default App;
-
- */
 
 import React from "react";
 import ScrumBoard from './ScrumBoard.jsx';
-//import './src/css/style.css'; // Add some basic styling
 
-const App = () => {
+ function App()  {
     return (
         <div className="App">
             <h1>Scrum Board</h1>
             <ScrumBoard />
         </div>
     );
-};
+}
 
 export default App;
